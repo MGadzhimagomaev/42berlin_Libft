bea4dfa44c048eaea5a5f24a3fdeeeb499e08a98
Marat Gadzhimagomaev <mgadzhim@c4b10c5.42berlin.de>
2025-05-16 19:02:11 +0200

    first functions

---
+++ libft.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgadzhim <mgadzhim@student.42berlin.d      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/15 20:51:31 by mgadzhim          #+#    #+#             */
+/*   Updated: 2025/05/15 21:45:04 by mgadzhim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+#include <stddef.h>
+
+int	ft_isalpha(int c);
+int	ft_isdigit(int c);
+int     ft_isalnum(int c);
+int	ft_isascii(int c);
+int	ft_isprint(int c);
+size_t  ft_strlen(const char *s);
+
+#endif
