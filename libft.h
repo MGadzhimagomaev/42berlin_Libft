0c39efc0d051d7efcc1f12ef01298f88cf536c06
Marat Gadzhimagomaev <mgadzhim@c4b3c2.42berlin.de>
2025-05-18 20:54:12 +0200

    atoi & strnstr

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mgadzhim <mgadzhim@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/15 20:51:31 by mgadzhim          #+#    #+#             */
-/*   Updated: 2025/05/17 18:37:22 by mgadzhim         ###   ########.fr       */
+/*   Updated: 2025/05/18 20:53:51 by mgadzhim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,5 +26,14 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 size_t	ft_strlcpy(char *dest, char *src, unsigned int size);
 size_t	ft_strlcat(char *dest, const char *src, size_t size);
+int	ft_toupper(int c);
+int	ft_tolower(int c);
+char	*ft_strchr(const char *s, int c);
+char	*ft_strrchr(const char *s, int c);
+int	ft_strncmp(const char *s1, const char *s2, size_t n);
+void	*ft_memchr(const void *s, int c, size_t n);
+int	ft_memcmp(const void *s1, const void *s2, size_t n);
+char	*ft_strnstr(const char *big, const char *little, size_t len);
+int	ft_atoi(const char *nptr);
 
 #endif
