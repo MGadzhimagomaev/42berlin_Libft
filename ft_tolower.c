9f04a8d55b15e327ba4579846e246a5174e8b8f4
Marat Gadzhimagomaev <mgadzhim@c4b3c2.42berlin.de>
2025-05-18 16:12:31 +0200

    ft_toupper / lower

---
+++ ft_tolower.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_tolower.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mgadzhim <mgadzhim@student.42berlin.de>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/18 15:59:33 by mgadzhim          #+#    #+#             */
+/*   Updated: 2025/05/18 16:11:30 by mgadzhim         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int	ft_tolower(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+		c += 32;
+	return (c);
+}
+
+
+#include <stdio.h>
+
+int	main()
+{
+	char	str[] = "tEsT ThIs";
+	int	i = 0;
+
+	printf("Before: %s\n", str);
+	while (i < 9)
+	{
+		str[i] = ft_tolower(str[i]);
+		i++;
+	}
+	printf("After: %s\n", str);
+}//*/
