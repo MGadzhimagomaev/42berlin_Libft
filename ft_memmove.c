9486be1efc0e019de35855c0a05349ae58b607eb
Marat Gadzhimagomaev <mgadzhim@c4b3c2.42berlin.de>
2025-05-18 15:53:09 +0200

    ft_memmove

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,30 +6,40 @@
 /*   By: mgadzhim <mgadzhim@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/16 21:34:40 by mgadzhim          #+#    #+#             */
-/*   Updated: 2025/05/17 17:31:00 by mgadzhim         ###   ########.fr       */
+/*   Updated: 2025/05/18 15:52:26 by mgadzhim         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-// #include "libft.h"
+#include "libft.h"
 
-// void	*ft_memmove(void *dest, const void *src, size_t n)
-// {
-// 	// const unsigned char* temp;
+void	*ft_memmove(void *dest, const void *src, size_t n)
+{
+	char	*d;
+	char	*s;
 
-// 	// temp = src;
-// 	// while (n-- > 0)
-// 	// {
-// 	// 	((unsigned char*)dest)[n] = temp[n];
-// 	// }
-// 	// return (dest);
-// }
+	d = (char*) dest;
+	s = (char *) src;
 
+	if (d > s)
+		while (n-- > 0)
+			d[n] = s[n];
+	else
+		while (*s)
+		{
+			*d = *s;
+			s++;
+			d++;
+		}
+	return (dest);
+}
+/*
 #include <stdio.h>
 #include <string.h>
 
 int main(void)
 {
 	char std_buf[30] = "Overlapping copy test!";
+	char std_buf_ft[30] = "Overlapping copy test!";
 	char std_buf3[30] = "\0";
 	char std_buf2[30] = "";
 	char std_buf4[30] = "X";
@@ -42,29 +52,48 @@ int main(void)
 
 	// Overlapping copy: copy 10 bytes from index 0 to index 5
 	memmove(std_buf + 5, std_buf, 10);	
+	ft_memmove(std_buf_ft + 5, std_buf_ft, 10);	
 	printf("\nAfter 1:\n");
-	printf("std_buf: %s\n", std_buf);
-	memmove(std_buf + 5, std_buf2, 10);
-	printf("\nAfter 2:\n");
-	printf("std_buf: %s\n", std_buf);
-	memmove(std_buf + 5, std_buf3, 10);
-	printf("\nAfter 3:\n");
-	printf("std_buf: %s\n", std_buf);
-	memmove(std_buf + 5, std_buf4, 10);
-	printf("\nAfter 4:\n");
-	printf("std_buf: %s\n", std_buf);
-	// ft_memmove(ft_buf + 5, ft_buf, 10);
-
-	// Print after
-	// printf("\nAfter:\n");
-	// printf("std_buf: %s\n", std_buf);
-	// printf("ft_buf : %s\n", ft_buf);
-
+	printf("Orig: %s\n", std_buf);
+	printf("ft_memmove: %s\n", std_buf_ft);
 	// Compare memory
-	// if (memcmp(std_buf, ft_buf, sizeof(std_buf)) == 0)
-	// 	printf("\n✅ Buffers match! ft_memmove works correctly.\n");
-	// else
-	// 	printf("\n❌ Buffers do not match. Bug detected in ft_memmove.\n");
+	if (memcmp(std_buf, std_buf_ft, sizeof(std_buf)) == 0)
+		printf("\n✅ Buffers match! ft_memmove works correctly.\n");
+	else
+		printf("\n❌ Buffers do not match. Bug detected in ft_memmove.\n");
+
+	memmove(std_buf + 5, std_buf2, 10);
+	ft_memmove(std_buf_ft + 5, std_buf2, 10);	
+	printf("\nAfter 2:\n");
+	printf("Orig: %s\n", std_buf);
+	printf("ft_memmove: %s\n", std_buf_ft);
+	// Compare memory
+	if (memcmp(std_buf, std_buf_ft, sizeof(std_buf)) == 0)
+		printf("\n✅ Buffers match! ft_memmove works correctly.\n");
+	else
+		printf("\n❌ Buffers do not match. Bug detected in ft_memmove.\n");
+
+	memmove(std_buf + 5, std_buf3, 10);
+	ft_memmove(std_buf_ft + 5, std_buf3, 10);	
+	printf("\nAfter 3:\n");
+	printf("Orig: %s\n", std_buf);
+	printf("ft_memmove: %s\n", std_buf_ft);
+	// Compare memory
+	if (memcmp(std_buf, std_buf_ft, sizeof(std_buf)) == 0)
+		printf("\n✅ Buffers match! ft_memmove works correctly.\n");
+	else
+		printf("\n❌ Buffers do not match. Bug detected in ft_memmove.\n");
+
+	memmove(std_buf + 5, std_buf4, 10);
+	ft_memmove(std_buf_ft + 5, std_buf4, 10);	
+	printf("\nAfter 4:\n");
+	printf("Orig: %s\n", std_buf);
+	printf("ft_memmove: %s\n", std_buf_ft);
+	// Compare memory
+	if (memcmp(std_buf, std_buf_ft, sizeof(std_buf)) == 0)
+		printf("\n✅ Buffers match! ft_memmove works correctly.\n");
+	else
+		printf("\n❌ Buffers do not match. Bug detected in ft_memmove.\n");
 
 	return 0;
-}
+}//*/
